3fd19795303b8f033bafdc33837e6e28d1301a48
Oleg Pykhonin <stoppard23@gmail.com>
2026-01-25 14:45:49 +0100

    refactor clerk: fix AccountPage and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { store } from './store/store';
 import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
 import './index.css';
+
+if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key');
+}
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,5 +1,9 @@
-
-import { UserProfile } from '@clerk/clerk-react';
+import {
+  UserProfile,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from '@clerk/clerk-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../features/theme/themeSlice';
 import type { RootState } from '../store/store';
@@ -12,13 +16,19 @@ const AccountPage = () => {
     <div className="relative min-h-screen px-10 py-14 dark:bg-black">
       <button
         onClick={() => dispatch(toggleTheme())}
-        className="absolute z-50 px-4 py-1 transition border-2 border-pink-400 rounded top-28 dark:border-gray-600 hover:bg-black hover:text-white dark:hover:bg-pink-500 dark:text-whitev right-96"
+        className="absolute z-50 px-4 py-1 transition border-2 border-pink-400 rounded top-28 right-96 dark:border-gray-600 hover:bg-black hover:text-white dark:hover:bg-pink-500 dark:text-white"
       >
         {theme === 'light' ? 'Dark' : 'Light'}
       </button>
 
       <div className="flex justify-center mt-10">
-        <UserProfile />
+        <SignedIn>
+          <UserProfile />
+        </SignedIn>
+
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
       </div>
     </div>
   );
